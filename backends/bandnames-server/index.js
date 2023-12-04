63082fa67a897e7ec38bb5e1a50d6f4b72308e37
PabloVR24 <pavara_2010@hotmail.com>
2023-12-04 14:43:05 -0600

    Fin seccion 3

--- backends/bandnames-server/index.js
+++ backends/bandnames-server/index.js
@@ -1,9 +1,37 @@
-import express from "express";
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+
 const app = express();
 
-app.listen(3000, (err) => {
+//! NODE SERVER
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// ! Sockets Messages
+
+io.on('connection', client => {
+    console.log('Cleinte conectado');
+    client.on('disconnect', () => {
+        console.log('Cleinte desconectado');
+     });
+
+     client.on('mensaje', (payload)=>{
+        console.log('Mensaje', payload);
+        io.emit('mensaje', {admin:'Nuevo mensaje'})
+     })
+  });
+
+
+//! PUBLIC PATH
+server.listen(process.env.PORT, (err) => {
   if (err) {
     throw new Error(err);
   }
-  console.log(`Servidor corriendo en el puerto: `, 3000);
+  console.log(`Servidor corriendo en el puerto: `, process.env.PORT);
 });
+
+
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
